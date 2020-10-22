ca83983f3feecd6d19235ae0facd525333b13b5e
pluto <g.wang.x@gmail.com>
2020-10-22 21:23:46 +0800

    f

--- server/src/tools/eth_wallet.ts
+++ server/src/tools/eth_wallet.ts
@@ -1,6 +1,9 @@
 
-import { MNEMONIC } from '../config/env';
-import { mnemonic } from '../helpers/mnemonic';
+import { process_init } from '../common/utils/process_init';
+process_init();
+
+import { MNEMONIC } from '@config/env';
+import { mnemonic } from '@helpers/mnemonic';
 
 async function run() {
     const address = await mnemonic.toAddress(MNEMONIC, "m/44'/60'/0'/0/" + 1);
