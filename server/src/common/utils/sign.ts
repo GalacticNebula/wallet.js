81753ef273e32e7c2f176e9621d4e149e53beff8
mr bee <you@example.com>
2020-11-23 10:46:25 +0800

    fix sign

--- server/src/common/utils/sign.ts
+++ server/src/common/utils/sign.ts
@@ -1,19 +1,7 @@
-
 import crypto from 'crypto';
 
-function dataToString(args: any) {
-  let result = '';
-  const keys = Object.keys(args).sort();
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    if (result.length > 0) result = result + '&';
-    result = result + key + '=' + args[key];
-  }
-
-  return result;
-}
-
 export function sign(args: any, secret: string) {
-  const hex = crypto.createHmac('sha256', secret).update(dataToString(args)).digest('hex');
-  return Buffer.from(hex).toString('base64');
+  const keys = Object.keys(args).sort();
+  const text = keys.map((k) => k + '=' + args[k]).join('&');
+  return crypto.createHmac('sha256', secret).update(text).digest('base64');
 }
