9815bf8b38a8529cb9b2a545b4f8af860b94dffd
maswalker <maswalker@github.com>
2020-12-10 06:14:24 +0000

    f

--- restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-NAME=walletjs
-
-docker container restart ${NAME}_api_1 && docker container restart ${NAME}_daemon_1
-
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-NAME=wallet
-
-git pull && cd server && npm run build && cd .. && docker container restart ${wallet}_api_1 && docker container restart ${wallet}_daemon_1
